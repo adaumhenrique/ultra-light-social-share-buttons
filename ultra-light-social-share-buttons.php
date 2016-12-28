d30a4ad45624062ba7594f06919b4789ae290594
Adam Silva <adam@adamsilva.com.br>
2016-12-28 00:21:40 -0200

    Changed to settings API

--- ultra-light-social-share-buttons.php
+++ ultra-light-social-share-buttons.php
@@ -1,24 +1,23 @@
 <?php
 /*
  * Plugin Name: Ultra Light Social Share Buttons
- * Plugin URI: http://www.estudarti.com.br
+ * Plugin URI: https://github.com/adaumhenrique/ultra-light-social-share-buttons
  * Description: Displays social share buttons on your posts. No JavaScript required, no images required, this plugin uses SVG.
  * Version: 1.0
  * Author: Adam Silva
- * Author URI: http://www.adamsilva.com.br
+ * Author URI: http://www.f5themes.com
  * License GPLv3
  */
 
 
-$plugin_url = WP_PLUGIN_URL . '/ultra-light-social-share-buttons';
 $options = array();
 
 function ultralight_menu() {
 	add_options_page(
-		'Ultra light social share buttons',
-		'Ultra light social',
+		'Ultra Light Social Share Buttons',
+		'Ultra Light Social',
 		'manage_options',
-		'ultralight',
+		'ultralight_fields',
 		'ultralight_options_page'
 	);
 }
@@ -29,54 +28,99 @@ function ultralight_options_page() {
 		wp_die( 'You do not have sufficiente permissions to access this page.' );
 	}
 
-	global $plugin_url;
-	global $options;
-
-	if ( isset( $_POST['form_submitted'] ) ) {
-		$hidden_field = esc_html( $_POST['form_submitted'] );
-		if ( 'Y' === $hidden_field ) {
-			$options['facebook'] = $_POST['facebook'];
-			$options['twitter'] = $_POST['twitter'];
-			$options['google'] = $_POST['google'];
-			$options['tumblr'] = $_POST['tumblr'];
-			$options['email'] = $_POST['email'];
-			$options['pinterest'] = $_POST['pinterest'];
-			$options['linkedin'] = $_POST['linkedin'];
-			$options['reddit'] = $_POST['reddit'];
-			$options['xing'] = $_POST['xing'];
-			$options['whatsapp'] = $_POST['whatsapp'];
-			$options['hacker'] = $_POST['hacker'];
-			$options['vk'] = $_POST['vk'];
-			$options['telegram'] = $_POST['telegram'];
-			$options['position'] = $_POST['position'];
-			update_option( 'ultralight_options', $options );
-		}
-	}
-
-	$options = get_option( 'ultralight_options' );
-	if ( '$options' != '' ) {
-		var_dump( $options );
-	}
-
 	require( 'includes/options-page-wrapper.php' );
 }
 
+function ultralight_setup_sections() {
+	add_settings_section( 'ultralight_display', 'Display Position', 'ultralight_section_callback', 'ultralight_fields' );
+	add_settings_section( 'ultralight_buttons', 'Disable / Enable Social Buttons', 'ultralight_section_callback', 'ultralight_fields' );
+}
+add_action( 'admin_init', 'ultralight_setup_sections' );
+
+function ultralight_section_callback( $arguments ) {
+	switch( $arguments['id'] ){
+		case 'ultralight_display':
+			esc_attr_e( 'Choose in which place of the blog post you want to display the social share buttons', 'wp_admin_style' );
+			break;
+		case 'ultralight_buttons':
+			echo 'Choose which social share buttons you want to display on your blog posts';
+			break;
+	}
+}
+
+function ultralight_setup_fields() {
+	$fields = array(
+		array(
+			'uid' => 'ultralight_position',
+			'label' => 'Buttons Position',
+			'section' => 'ultralight_display',
+			'type' => 'radio',
+			'options' => array(
+				'top' => 'Top',
+				'bot' => 'Bottom',
+				'both' => 'Both',
+			),
+			'default' => array(),
+		),
+		array(
+			'uid' => 'ultralight_buttons',
+			'label' => 'Social Buttons',
+			'section' => 'ultralight_buttons',
+			'type' => 'checkbox',
+			'options' => array(
+				'facebook' => 'Facebook',
+				'twitter' => 'Twitter',
+				'google' => 'Google+',
+				'linkedin' => 'Linkedin',
+				'tumblr' => 'Tumblr',
+				'email' => 'Email',
+				'pinterest' => 'Pinterest',
+				'reddit' => 'Reddit',
+				'xing' => 'Xing',
+				'whatsapp' => 'Whatsapp',
+				'vk' => 'Vk',
+				'telegram' => 'Telegram',
+			),
+			'default' => array(),
+		),
+	);
+	foreach ( $fields as $field ) {
+		add_settings_field( $field['uid'], $field['label'], 'ultralight_field_callback', 'ultralight_fields', $field['section'], $field );
+		register_setting( 'ultralight_fields', $field['uid'] );
+	}
+}
+add_action( 'admin_init', 'ultralight_setup_fields' );
+
+function ultralight_field_callback( $arguments ) {
+	$value = get_option( $arguments['uid'] );
+	if ( ! $value ) {
+		$value = $arguments['default'];
+	}
+	switch ( $arguments['type'] ) {
+		case 'radio':
+		case 'checkbox':
+			if ( ! empty( $arguments['options'] ) && is_array( $arguments['options'] ) ) {
+				$options_markup = '';
+				$iterator = 0;
+				foreach ( $arguments['options'] as $key => $label ) {
+					$iterator++;
+					$options_markup .= sprintf( '<label for="%1$s_%6$s"><input id="%1$s_%6$s" name="%1$s[]" type="%2$s" value="%3$s" %4$s /> %5$s</label><br/>', $arguments['uid'], $arguments['type'], $key, checked( $value[ array_search( $key, $value, true ) ], $key, false ), $label, $iterator );
+				}
+				printf( '<fieldset>%s</fieldset>', $options_markup );
+			}
+			break;
+	}
+}
+
 function ultralight_set_defaults() {
-	$options['facebook'] = '1';
-	$options['twitter'] = '1';
-	$options['google'] = '1';
-	$options['linkedin'] = '1';
-	$options['tumblr'] = '0';
-	$options['email'] = '0';
-	$options['pinterest'] = '0';
-	$options['reddit'] = '0';
-	$options['xing'] = '0';
-	$options['whatsapp'] = '0';
-	$options['hacker'] = '0';
-	$options['vk'] = '0';
-	$options['telegram'] = '0';
-	$options['position'] = 'top';
-	update_option( 'ultralight_options', $options );
+	$buttons[0] = 'facebook';
+	$buttons[1] = 'twitter';
+	$buttons[2] = 'google';
+	$buttons[3] = 'linkedin';
+	update_option( 'ultralight_buttons', $buttons );
+
+	$position[0] = 'top';
+	update_option( 'ultralight_position', $position );
 }
 register_activation_hook( __FILE__, 'ultralight_set_defaults' );
 
@@ -87,8 +131,19 @@ function ultralight_show_buttons_post( $post ) {
 		require( 'includes/social-buttons.php' );
 	}
 
-	$content .= $post;
-	return $content;
+	$position = get_option( 'ultralight_position' );
+
+	switch ( $position[0] ) {
+		case 'top' :
+			return $content . $post;
+		break;
+		case 'bot' :
+			return $post . $content;
+		break;
+		case 'both' :
+			return $content . $post . $content;
+		break;
+	}
 }
 add_filter( 'the_content', 'ultralight_show_buttons_post' );
 
